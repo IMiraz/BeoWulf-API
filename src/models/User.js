aa578b5d6d365eee70c061edcc4c83e78966a8e3
wasifmiraz <wasifmiraz@mail.com>
2018-08-20 19:25:59 +0600

    models uo

---
+++ src/models/User.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
+
+
+const schema = new mongoose.Schema({
+email:{
+    type:String,
+    required:true,
+    lowercase:true,
+    index:true,
+    unique:true
+},
+passwordHash:{
+    type:String,
+    required:true
+},
+confirmed:{
+    type:Boolean,
+    default:false
+}
+
+}, {timestamps:true});
+
+schema.methods.isValidPassword = function isValidPassword(password) {
+    return bcrypt.compareSync(password,this.passwordHash);
+
+};
+
+schema.methods.setPassword = function setPassword(password) {
+    this.passwordHash = bcrypt.hashSync(password, 10);
+  };
+
+schema.methods.genarateJWT = function genarateJWT() {
+ return jwt.sign({
+ email:this.email
+ },process.env.JWT_SECRETKEY)
+};
+schema.methods.toAuthJSON = function toAuthJSON() {
+     return{
+    email: this.email,
+    confirmed:this.confirmed,
+    token:this.genarateJWT()
+     }
+
+};
+
+schema.plugin(uniqueValidator, {message:"this email already taken"})
+
+
+
+export default mongoose.model('User', schema);
