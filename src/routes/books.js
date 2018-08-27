4c09b8dbf7bd02f2489c53ffa7640fe2c4c55ad4
wasifmiraz <wasifmiraz@mail.com>
2018-08-27 17:52:07 +0600

    api up

--- src/routes/books.js
+++ src/routes/books.js
@@ -10,7 +10,9 @@ books:[
 title:"three Men in a boat",
 authors:"json marler",
 covers:[
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1WZRYhU3vJziUtl0NBvrlU8fbnAcZoWj5iEM1R9oaSLR_7O4-"
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1WZRYhU3vJziUtl0NBvrlU8fbnAcZoWj5iEM1R9oaSLR_7O4-",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYf1ZplGTwHsHblmMWDFtAyLK390kXlUe_dEuk4RzK0wybpUQj",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrBfiJ9k8PER_UIfleu1INn8Yke7_qwuMiNRkeWnaCa9OHpwqj"
 
 ],
 pages:198
@@ -21,7 +23,10 @@ pages:198
    authors:"orwall",
    covers:[
 
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjxju_Qu7wa7G_f37IDxIZZJjZHQZcooz-FDYVL_LN7mN3tDXC"
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjxju_Qu7wa7G_f37IDxIZZJjZHQZcooz-FDYVL_LN7mN3tDXC","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj-K5QBCyGSZQ59z2W4S3Zb71Yw9vam4HUzx8MBRgAWHThUe6-mA",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNr15FB2VcTzUG4lYsaPYAzBd7vdwUWmWMh4pBJkjjIM4baTHe"
+
+
 
    ],
    pages:198
